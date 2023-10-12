18ba13b4fe66a1ffc9e1f3fdd101febb904e1ca0
Lorenzo De Angelis <125358763+NotLor@users.noreply.github.com>
2023-10-12 11:52:27 +0000

    z

--- esSetteMezzo/index.php
+++ esSetteMezzo/index.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sette Mezzo</title>
+    <link rel="stylesheet" href="styleGame.css">
 </head>
 <body>
-
+    <div class="container">
+        <div class="banco">
+            <h1>BANCO</h1>
+        </div class="player">
+        <div>
+            <h1>TU</h1>
+        </div>
+    </div>
 </body>
 </html>
